8e204d3f14b0b3611bbb8f46c93994fdd207c44a
Ryan Panning <panman82@gmail.com>
2014-12-29 12:52:50 -0600

    Add bower deps with one call
    Make use of the .addBowerPackagesToProject() feature introduced in
    ember-cli 0.1.2. Also keep backwards compatibility with 0.0.41 by
    feature detecting.

--- blueprints/ember-cli-bootstrap/index.js
+++ blueprints/ember-cli-bootstrap/index.js
@@ -8,9 +8,17 @@ module.exports = {
   afterInstall: function() {
     var addonContext = this;
 
-    return this.addBowerPackageToProject('bootstrap', '~3.3')
-      .then(function() {
+    // Ability to add multiple bower packages introduced in ember-cli 0.1.2
+    if (this.addBowerPackagesToProject) {
+      return this.addBowerPackagesToProject([
+        {name: 'bootstrap', target: '~3.3'},
+        {name: 'ember-addons.bs_for_ember', target: '~0.7.0'}
+      ]);
+    } else { // Else need to add them individually
+      return this.addBowerPackageToProject('bootstrap', '~3.3').then(function(){
         return addonContext.addBowerPackageToProject('ember-addons.bs_for_ember', '~0.7.0');
       });
+    }
+
   }
 };
